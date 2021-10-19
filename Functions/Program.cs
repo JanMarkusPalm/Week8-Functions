d5ce0d9816d6da59cd34999992a60c6eeaccc67c
Opilane TTHK <opilane@tthk.ee>
2021-10-19 11:25:38 +0300

    functions

---
+++ Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta oma nimi.");
+            string username = Console.ReadLine();
+            Console.WriteLine("sisesta oma vanus");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            SayHello(username, userAge);
+        }
+
+    private static void SayHello(string someName, int someNumber)
+        {
+            Console.WriteLine($"Hi There {someName}");
+            Console.WriteLine($"oled sündinud aastal {2021 - someNumber}.");
+        }
+
+
+
+    }
+}
